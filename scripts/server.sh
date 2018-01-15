99d09d6c992fb70f7cbe4a34212af47666570ffc
Simon Brouard <brouard.simon@wanadoo.fr>
2018-01-15 19:17:59 +0100

    added var in server.sh

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
 
 
+if [ "$#" -ne 2 ]; then
+    echo "Use: ./server.sh screenWidth screenHeight"
+else
 
+screenSize="$1x$2"  #1600x900
+frameRate="30"
+input=":1"
+if [ $DISPLAY ]; then
+    input=$DISPLAY
+fi
 
-ffmpeg -f x11grab -s 1600x900 -framerate 30 -i :1 -c:v libx264 -preset veryfast -tune zerolatency -pix_fmt yuv444p -x264opts crf=20:vbv-maxrate=3000:vbv-bufsize=100:intra-refresh=1:slice-max-size=1500:keyint=30:ref=1 -f mpegts - | nc -l -p 9000
+codecVideo="libx264"
+pixelFormat="yuv444p"
+compressionRate="20" #between 0 and 51 ; Higher values mean more compression, but quality degradation.
+maximumBitRate="10000"
+bufferSize="100"
 
+port="9000"
 
+ffmpeg -f x11grab -s $screenSize -framerate $frameRate -i $input -c:v $codecVideo -preset veryfast -tune zerolatency -pix_fmt $pixelFormat -x264opts crf=$compressionRate:vbv-maxrate=$maximumBitRate:vbv-bufsize=$bufferSize:intra-refresh=1:keyint=30:slice-max-size=1500:ref=1 -f mpegts - | nc -l -p $port 
 
-# -f format (x11grab: capture ecran)
+fi
+
+# -f format (x11grab: screen capture)
 # -s size
 # -i input file (echo $DISPLAY)
-# -c ???
+# -c:v codec (v for video, codec is the encoding method)
+# -pix_fmt (pixel encoding format)
+# -f mpegts (output format)
+# crf
